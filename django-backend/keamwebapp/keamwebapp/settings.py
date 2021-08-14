77639f59092cf4c68482bfbee9d951619df92f7a
Aman K Shihab <amanshihab276@gmail.com>
2021-08-15 00:40:42 +0530

    Revert "Fixed security issue"
    This reverts commit 83eb4a5b7044b2f2ce4fd248b4887822fb2f531e.

--- django-backend/keamwebapp/keamwebapp/settings.py
+++ django-backend/keamwebapp/keamwebapp/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-import os
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-0-$vpdus2%xdkv5vpm$-r82#5=uz$n9d&yamx0gogqv*ev)!dx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
