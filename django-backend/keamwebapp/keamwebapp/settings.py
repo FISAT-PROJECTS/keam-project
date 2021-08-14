408a1c91742e5fd2b6258d9738ce2bda18950888
Aman K Shihab <amanshihab276@gmail.com>
2021-08-14 23:29:25 +0530

    Finished basic web app

--- django-backend/keamwebapp/keamwebapp/settings.py
+++ django-backend/keamwebapp/keamwebapp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'college_predictor',
+    
     'crispy_forms',
 ]
 
